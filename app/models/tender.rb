70e1d4263d8959468b85d49682a3301400d27ef1
parwej akhtar <planetckp@gmail.com>
2016-05-24 21:06:18 +0530

    coonfigure dropbox

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,11 +1,14 @@
 class Tender < ActiveRecord::Base
+if Rails.env.devlopmet?
+has_attached_file :document
 
+else	
 has_attached_file :document,
 				:storage => :dropbox,
     			:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
 validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 
-   				      
+   end				      
 end
 
